952f240d5653c9921a098d9a57aff5d27e328c34
AntanasGa <a.gargasas1238@gmail.com>
2023-11-04 01:05:00 +0200

    renaming token

--- src/util/MdParser/TokenModels/CodeToken.ts
+++ src/util/MdParser/TokenModels/CodeToken.ts
@@ -2,7 +2,7 @@ import { TokenHandlerV2 } from "../types";
 import ReverseEscape from "../../string/ReverseEscape";
 
 const CodeToken: TokenHandlerV2<"code"> = function (token, container) {
-  const currentContext = container.push("pre");
+  const currentContext = container.push("code");
   currentContext.context.push(ReverseEscape(token.text))
   return container;
 }
