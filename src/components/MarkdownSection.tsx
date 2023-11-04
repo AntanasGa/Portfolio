3900c1cdfb6c789851cff64e6831f51050a2c5ae
AntanasGa <a.gargasas1238@gmail.com>
2023-11-04 14:43:10 +0200

    markdown section cleanup

--- src/components/MarkdownSection.tsx
+++ src/components/MarkdownSection.tsx
@@ -2,13 +2,7 @@ import { useMemo } from "react";
 import MdParser from "../util/MdParser";
 
 function MarkdownSection({ markdown, testId }: { markdown: string, testId?: string }) {
-  const items = useMemo(() => {
-    // const start = performance.now(); 
-    const parsed = MdParser(markdown);
-    // const end = performance.now();
-    // console.log(end - start);
-    return parsed;
-  }, [markdown]);
+  const items = useMemo(() => MdParser(markdown), [markdown]);
   return (
     <section style={{ width: "100vw" }} data-testid={testId}>{ items }</section>
   );
