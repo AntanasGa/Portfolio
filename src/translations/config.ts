5cd6c59bf8bc1f9f8eace6eca05e06d16869ceea
AntanasGa <anatanas.ga@gmail.com>
2024-01-25 18:19:28 +0200

    expanding on locale configuration

--- src/translations/config.ts
+++ src/translations/config.ts
@@ -1,3 +1,5 @@
 export const DEFAULT_LANGUAGE = "en";
 
-export const LANGUAGE_LIST = [DEFAULT_LANGUAGE, "lt"];
+export const LANGUAGE_MAP = { [DEFAULT_LANGUAGE]: !0, "lt": !0 };
+
+export const LANGUAGE_LIST = Object.keys(LANGUAGE_MAP);
