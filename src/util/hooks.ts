7ab6102ca72ca77fd82e6d83802f2b22c01009ee
AntanasGa <anatanas.ga@gmail.com>
2024-01-25 18:13:16 +0200

    adding missing hook file

---
+++ src/util/hooks.ts
@@ -0,0 +1,9 @@
+import abstractReducerFunction from "./reducer/abstractReducerFunction";
+import createReducerInitializer from "./reducer/createReducerInitialize";
+import useReducerMask from "./reducer/useReducerMask";
+
+export default {
+  abstractReducerFunction,
+  createReducerInitializer,
+  useReducerMask,
+};
