4ed69fbd262bcf0a48089d3500a55df9ba56f9db
AntanasGa <anatanas.ga@gmail.com>
2024-02-08 22:09:24 +0200

    fixing markdown tests

--- src/tests/components/MarkdownSection/helpers/fixture.tsx
+++ src/tests/components/MarkdownSection/helpers/fixture.tsx
@@ -1,8 +1,9 @@
 import { render } from '@testing-library/react';
-import MarkdownSection from '../../../../components/MarkdownSection';
-import { ConfigContext } from '../../../../util/MdParser/types';
+import MarkdownSection from '~/components/MarkdownSection';
+import { ConfigContext } from '~/util/MdParser/types';
+import { uuidv4 } from '~/util/string/Guid';
 
 export const getRender = (content: string = "", config?: ConfigContext) => {
-  const testId = "initializer-empty"
+  const testId = uuidv4();
   return {testId, render: render(<MarkdownSection markdown={ content } testId={ testId } config={ config } />)};
 }
