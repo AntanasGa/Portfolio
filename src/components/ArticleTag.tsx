b2bcc07c455c9a5b5fe807db820eabce68c2a6fc
AntanasGa <anatanas.ga@gmail.com>
2024-02-08 22:20:56 +0200

    adding content type

---
+++ src/components/ArticleTag.tsx
@@ -0,0 +1,32 @@
+import { useTranslation } from "react-i18next";
+import { IContentTagItem } from "../reducers/manifest";
+import { HOST } from "~/util/cdn/constants";
+import { useMemo } from "react";
+import hexColorParser from "~/util/string/HexColorParser";
+
+export default function ArticleTag({ item }: { item: IContentTagItem }) {
+  const { t } = useTranslation("pages", { keyPrefix: "index" });
+  const paragraphStyle = useMemo(
+    () => {
+      const val = hexColorParser(item.background ?? "FFFFFF");
+      // thanks to react team for this
+      return Object.fromEntries([[ "--background-color", (val ? val : "initial") ]])
+    },
+    [ item ]
+  );
+  return (
+    <div className="tag">
+      <button style={ paragraphStyle }>
+        { item.logo &&
+          <img src={ (new URL(item.logo, HOST)).toString() } />
+        }
+        <span>{ item.type }</span>
+      </button>
+      { item.href &&
+        <a title={ t("linkToOrigins") } href={ item.href } target="_blank" rel="noreferrer noopener" >
+          <img src="/assets/link.svg" className="icon mono" />
+        </a>
+      }
+    </div>
+  );
+}
