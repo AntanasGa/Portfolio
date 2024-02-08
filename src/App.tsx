bfab905071f96c111cd060a61df594c1d83e7206
AntanasGa <anatanas.ga@gmail.com>
2024-02-08 22:16:38 +0200

    cleanup

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import { useTranslation } from 'react-i18next';
-
-function App() {
-  const { t } = useTranslation("pages", { keyPrefix: "index" });
-  return <div>{t('hello')}</div>;
-}
-
-export default App
