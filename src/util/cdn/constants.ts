e4d79dd8b1edb00c1d63775eb2addb143f30368e
AntanasGa <anatanas.ga@gmail.com>
2024-04-10 20:06:51 +0300

    Add CDN constant for content URL

--- src/util/cdn/constants.ts
+++ src/util/cdn/constants.ts
@@ -1,1 +1,3 @@
 export const HOST = import.meta.env.DEV ? new URL("/cdn/", window.location.origin) : new URL("https://cdn.antanasga.lt/");
+/** `${cdn}/content/` */
+export const CONTENT = new URL("content/", HOST);
