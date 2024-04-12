e307ba9d6e3edc41036732b2e084892aa45ce352
AntanasGa <anatanas.ga@gmail.com>
2024-04-12 15:23:43 +0300

    adding `data-lang` and its tests

--- src/tests/components/MarkdownSection/parts/code.ts
+++ src/tests/components/MarkdownSection/parts/code.ts
@@ -16,5 +16,24 @@ ${helloContent}
   expect(Array.from(section.children).length).toBe(1);
   
   expect(codeSpanList.length).toBe(1);
+  expect(firstOrUndefinedOf(codeSpanList)?.getAttribute("data-lang")).toBe(null);
+  expect(firstOrUndefinedOf(codeSpanList)?.innerHTML).toBe(`${helloContent}`);
+});
+
+it.concurrent("Should have code with hello and lang", ({ expect }) => {
+  const lang = "ts";
+  const { testId, render } = getRender(`\`\`\`${lang}
+${helloContent}
+\`\`\``);
+  const section = render.getByTestId(testId);
+  
+  const codeSpanList = Array.from(section.querySelectorAll("code") ?? []);
+  
+
+  expect(section).toBeDefined();
+  expect(Array.from(section.children).length).toBe(1);
+  
+  expect(codeSpanList.length).toBe(1);
+  expect(firstOrUndefinedOf(codeSpanList)?.getAttribute("data-lang")).toBe(lang);
   expect(firstOrUndefinedOf(codeSpanList)?.innerHTML).toBe(`${helloContent}`);
 });
--- src/util/MdParser/TokenModels/CodeToken.ts
+++ src/util/MdParser/TokenModels/CodeToken.ts
@@ -2,7 +2,12 @@ import { TokenHandler } from "../types";
 import ReverseEscape from "../../string/ReverseEscape";
 
 const CodeToken: TokenHandler<"code"> = function (token, container) {
-  const currentContext = container.push("code");
+  const attributeMap: Record<string, unknown> = {};
+  if (token.lang) {
+    attributeMap["data-lang"] = token.lang;
+  }
+  
+  const currentContext = container.push("code", Object.keys(attributeMap).length ? attributeMap : undefined);
   currentContext.context.push(ReverseEscape(token.text))
   return container;
 }
