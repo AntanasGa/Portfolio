acee5c66caf7dac4c949aabbb6a4a2e56411add0
AntanasGa <anatanas.ga@gmail.com>
2024-05-02 21:22:22 +0300

    adding relevant info to the function

--- src/util/number/random.ts
+++ src/util/number/random.ts
@@ -1,3 +1,9 @@
+/**
+ * 
+ * @param min Integer
+ * @param max Integer
+ * @returns Integer
+ */
 function random(min: number, max: number) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
